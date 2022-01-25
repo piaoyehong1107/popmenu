9b81f10120e6eb52c6e0ad0f99fed32fc2fe855b
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2022-01-24 23:24:37 -0600

    function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import items from "./data";
+import { v4 } from 'uuid'
+// import logo from "./logo.JPG";
 
 function App() {
+  const [typeValue, setType] = useState("");
+  const [menuItems, setMenuItems] = useState(items);
+  const deleteItem = (e) => {
+    setMenuItems(items.filter(item => item.id !== e.id))
+  }
+  const handleClick = () => {
+    setMenuItems(
+      [...items,
+      {
+        id: v4(),
+        memo: typeValue
+      }
+      ]
+    )
+    setType('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+      <div className="header">
+        <input
+          value={typeValue}
+          onChange={(e) => setType(e.target.value)}
+        ></input>
+        <button onClick={handleClick} >ADD</button>
+      </div>
+        <div className="title">
+          {/* <img src={logo} alt="logo" className="logo" /> */}
+          <h2>Popmenu</h2>
+          <div className="underline"></div>
+        </div>
+        <Menu 
+          items={menuItems} 
+          deleteItem={deleteItem}
+        />
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PopMenu from "./PopMenu";
+
 
 ReactDOM.render(
   <div>
-      <PopMenu/>
+      <App />
   </div>,
   document.getElementById('root')
 );
