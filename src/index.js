dfeb173faa26ea67ac3554c9b2d48584cf188642
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2022-01-30 16:28:20 -0600

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Link, Route, Switch, Redirect } from "react-router-dom";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ ()=>{
-        return(
-          <App />
-        )
-      }} />
+      <Route
+        exact
+        path="/"
+        component={() => {
+          return <App />;
+        }}
+      />
     </Switch>
-</BrowserRouter>,
+  </BrowserRouter>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
